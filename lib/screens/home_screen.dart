3f51bc4f564c33afcb4fcea4bc5639146b619430
amin <aminullahtajmuhammad@gmail.com>
2019-11-20 03:51:43 +0500

    Added Main/Home Screen with nested ListViews

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,19 +15,167 @@ class LunchState extends State<HomeScreen> {
 
   Widget initScreen(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.symmetric(vertical: 20.0),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        elevation: 5.0,
+        leading: Icon(
+          Icons.menu,
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+              Icons.search,
+              size: 26.0,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+                Icons.favorite
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+              Icons.shopping_cart,
+            ),
+          ),
+        ],
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
+      ),
+      body:
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 10.0),
         child: ListView(
-          scrollDirection: Axis.horizontal,
           children: <Widget>[
-            items("images/mask_group_5.png", "Hello World", "Hello Sub Title"),
-            items("images/mask_group_5.png", "Hello World", "Hello Sub Title"),
-            items("images/mask_group_5.png", "Hello World", "Hello Sub Title"),
-            items("images/mask_group_5.png", "Hello World", "Hello Sub Title"),
-            items("images/mask_group_5.png", "Hello World", "Hello Sub Title"),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                    child: Text(
+                      "Cetegories",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+
+                    },
+                    child: Text(
+                      "See All",
+                      style: TextStyle(
+                        color: Colors.deepPurple
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: itemsList(),
+            ), // Items List Container
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                    child: Text(
+                      "Featured",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+
+                    },
+                    child: Text(
+                      "See All",
+                      style: TextStyle(
+                          color: Colors.deepPurple
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: itemsList(),
+            ), // Items List Container
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                    child: Text(
+                      "Best Sale",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+
+                    },
+                    child: Text(
+                      "See All",
+                      style: TextStyle(
+                          color: Colors.deepPurple
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: itemsList(),
+            ) // Items List Container
           ],
-        )
+        )  // Main/Parent List View
       )
+    );
+  }
+
+  Container itemsList() {
+    return new Container(
+      height: 250.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          items("images/mask_group_5.png", "Item Price", "Item Desc "),
+          items("images/mask_group_6.png", "Item Price", "Item Desc "),
+          items("images/mask_group_7.png", "Item Price", "Item Desc "),
+          items("images/mask_group_8.png", "Item Price", "Item Desc "),
+          items("images/mask_group_10.png", "Item Price", "Item Desc "),
+          items("images/mask_group_11.png", "Item Price", "Item Desc "),
+          items("images/mask_group_12.png", "Item Price", "Item Desc "),
+          items("images/mask_group_5.png", "Item Price", "Item Desc "),
+          items("images/mask_group_6.png", "Item Price", "Item Desc "),
+        ],
+      ), // Child ListView
     );
   }
 
@@ -36,15 +184,25 @@ class LunchState extends State<HomeScreen> {
     return new Container(
       padding: EdgeInsets.all(10.0),
       child: SizedBox(
-        width: 100.0,
+        width: 130.0,
         child: Wrap(
           children: <Widget>[
             Image(
               image: image
             ),
             ListTile(
-              title: Text(title),
-              subtitle: Text(subTitle),
+              title: Text(
+                title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              subtitle: Text(
+                subTitle,
+                style: TextStyle(
+                  fontSize: 12.0
+                ),
+              ),
             )
           ],
         ),
@@ -53,7 +211,6 @@ class LunchState extends State<HomeScreen> {
   }
 
   Container shoppingItems(String imgSrc, String title, String subTitle) {
-    AssetImage image = new AssetImage(imgSrc);
     return new Container(
       width: 150.0,
       child: InkWell(
