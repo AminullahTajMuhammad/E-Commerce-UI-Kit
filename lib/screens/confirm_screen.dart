d48ebf658ca4956f5b148b7034969a21a09fd5fc
amin <aminullahtajmuhammad@gmail.com>
2019-11-26 02:25:26 +0500

    Added Profile Screen

--- lib/screens/confirm_screen.dart
+++ lib/screens/confirm_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'profile_screen.dart';
 
 class ConfirmScreen extends StatefulWidget {
   @override
@@ -43,7 +44,41 @@ class LunchState extends State<ConfirmScreen> {
                     color: Colors.black
                 ),
               ),
-            )
+            ),
+
+            Center(
+                child: Container(
+                    margin: EdgeInsets.only(top: 50.0, bottom: 10.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProfileScreen()));
+                      }, // Click Listener
+
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                      padding: const EdgeInsets.all(0.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [new Color(0xff374ABE), new Color(0xff64B6FF)],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(5.0))
+                        ),
+                        child: Container(
+                          constraints: const BoxConstraints(maxWidth: 300.0, minHeight: 40.0), // min sizes for Material buttons
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'Add Address',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                )
+            ),
           ],
         )
       ],
