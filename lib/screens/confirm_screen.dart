724ea26fa448377668c6e3007a4a89f99eb08338
amin <aminullahtajmuhammad@gmail.com>
2019-11-26 02:18:59 +0500

    Added Confirmation Screen

--- lib/screens/confirm_screen.dart
+++ lib/screens/confirm_screen.dart
@@ -11,7 +11,45 @@ class ConfirmScreen extends StatefulWidget {
 class LunchState extends State<ConfirmScreen> {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      body: initScreen(),
+    );
   }
+
+  initScreen() {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage("images/app_logo.png"),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            Text(
+              "Confirmation",
+              style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.black
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "You have successfully completed your payment",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+
 
 }
